7f2d361102c283eee51137b0c4c98d9c53bd17ef
123falksdfj <changnan666@gmail.com>
2020-10-26 14:11:05 +0800

    添加更新日志、制定更新计划

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
-let enable = false
-const v1 = uuid.v1
+let enable = false;
+const v1 = uuid.v1;
 
-const FORMELEMENT = 'input,textarea,select,radio'
+const FORMELEMENT = "input,textarea,select,radio";
 
-const rightFormElement = ele => {
-	if (ele.tagName === 'A' || ele.tagName === 'P') return false
-	return FORMELEMENT.indexOf(ele.tagName.toLocaleLowerCase()) !== -1
-}
+const rightFormElement = (ele) => {
+  if (ele.tagName === "A" || ele.tagName === "P") return false;
+  return FORMELEMENT.indexOf(ele.tagName.toLocaleLowerCase()) !== -1;
+};
 
 // 给下一个兄弟元素注入索引
 const injectPathForNextElement = (ele, path, paths) => {
-	if (!ele) return
-	if (rightFormElement(ele)) {
-		let lastPath = Number(path.slice(-1)) + 1
-		let finalPath = path.slice(0, -1) + lastPath
-		paths.push(finalPath)
-		ele.dataset.autofillPath = finalPath
-	} else {
-		injectPathForNextElement(ele.nextElementSibling, path, paths)
-	}
-}
+  if (!ele) return;
+  if (rightFormElement(ele)) {
+    let lastPath = Number(path.slice(-1)) + 1;
+    let finalPath = path.slice(0, -1) + lastPath;
+    paths.push(finalPath);
+    ele.dataset.autofillPath = finalPath;
+  } else {
+    injectPathForNextElement(ele.nextElementSibling, path, paths);
+  }
+};
 
 // 给所有表单元素注入 dom 层级索引
 const injectPathForFormElement = (children, path, paths = []) => {
-	Array.from(children).forEach((item, i) => {
-		const { children: itemChild } = item
-		injectPathForNextElement(item.nextElementSibling, path, paths)
+  Array.from(children).forEach((item, i) => {
+    const { children: itemChild } = item;
+    injectPathForNextElement(item.nextElementSibling, path, paths);
 
-		if (itemChild && itemChild.length) {
-			injectPathForFormElement(itemChild, path + '-' + i, paths)
-		} else {
-			if (!item.dataset.autofillPath) {
-				if (rightFormElement(item)) {
-					paths.push(path)
-					item.dataset.autofillPath = path
-				}
-			}
-		}
-	})
+    if (itemChild && itemChild.length) {
+      injectPathForFormElement(itemChild, path + "-" + i, paths);
+    } else {
+      if (!item.dataset.autofillPath) {
+        if (rightFormElement(item)) {
+          paths.push(path);
+          item.dataset.autofillPath = path;
+        }
+      }
+    }
+  });
 
-	return paths
-}
+  return paths;
+};
 
 // 恢复数据
-const restoreDataFromStorage = paths => {
-	paths.forEach(path => {
-		const target = document.querySelector(`[data-autofill-path="${path}"]`)
-		if (target) {
-			let cache = localStorage.getItem(path)
-			if (cache) {
-				target.value = cache
-			}
-		}
-	})
-}
+const restoreDataFromStorage = (paths) => {
+  paths.forEach((path) => {
+    const target = document.querySelector(`[data-autofill-path="${path}"]`);
+    if (target) {
+      let cache = localStorage.getItem(path);
+      if (cache) {
+        target.value = cache;
+      }
+    }
+  });
+};
 
 // 创建启动按钮
 const createElement = () => {
-	const div = document.createElement('div')
-	div.innerText = 'AutoFill'
-	div.style.cssText = `
+  const div = document.createElement("div");
+  div.innerText = "AutoFill";
+  div.style.cssText = `
 	position: fixed;
 	z-index: 99999999;
 	right: 50px;
@@ -74,42 +74,42 @@ const createElement = () => {
 	cursor: pointer;
 	box-shadow: rgb(101 82 255) 0 0 10px;
 	user-select: none;
-	`
+	`;
 
-	document.body.append(div)
+  document.body.append(div);
 
-	return div
-}
+  return div;
+};
 
 // 记住
-const onRemember = id => {
-	const target = document.querySelector(`[data-autofill="${id}"]`)
-	if (target) {
-		const key = target.dataset.autofillPath
-		localStorage.setItem(key, target.value)
-	}
-}
+const onRemember = (id) => {
+  const target = document.querySelector(`[data-autofill="${id}"]`);
+  if (target) {
+    const key = target.dataset.autofillPath;
+    localStorage.setItem(key, target.value);
+  }
+};
 
 // 清除
-const onClear = id => {
-	const target = document.querySelector(`[data-autofill="${id}"]`)
-	if (target) {
-		const key = target.dataset.autofillPath
-		localStorage.removeItem(key)
-	}
-}
+const onClear = (id) => {
+  const target = document.querySelector(`[data-autofill="${id}"]`);
+  if (target) {
+    const key = target.dataset.autofillPath;
+    localStorage.removeItem(key);
+  }
+};
 
 // 记住和清楚的浮窗
 const createFloatWindow = (x, y, id) => {
-	let div = document.getElementById(id)
+  let div = document.getElementById(id);
 
-	if (!div) {
-		div = document.createElement('div')
-		document.body.append(div)
-	}
+  if (!div) {
+    div = document.createElement("div");
+    document.body.append(div);
+  }
 
-	div.id = id
-	div.style.cssText = `
+  div.id = id;
+  div.style.cssText = `
 	width: 100px;
 	height:	40px;
 	display: flex;
@@ -121,67 +121,56 @@ const createFloatWindow = (x, y, id) => {
 	top: ${y - 40}px;
 	background: blue;
 	color: #fff;
-`
+`;
 
-	const remember = $(
-		`<span style="margin-right: 10px;padding-right: 10px;border-right: 1px solid #ddd;cursor: pointer">记住</span>`
-	).on('click', () => onRemember(id))
+  const remember = $(
+    `<span style="margin-right: 10px;padding-right: 10px;border-right: 1px solid #ddd;cursor: pointer">记住</span>`
+  ).on("click", () => onRemember(id));
 
-	const clear = $(`<span style="cursor: pointer">清除</span>`).on('click', () =>
-		onClear(id)
-	)
+  const clear = $(`<span style="cursor: pointer">清除</span>`).on("click", () =>
+    onClear(id)
+  );
 
-	$(div).html('')
-	$(div).append(remember, clear)
+  $(div).html("");
+  $(div).append(remember, clear);
 
-	return div
-}
+  return div;
+};
 
 // 控制浮窗的显示状态
-const controllAllForm = use => {
-	const allForm = [...document.querySelectorAll(FORMELEMENT)]
-		.filter(
-			ele => ele.type !== 'submit' && (ele.className !== '' || ele.id !== '')
-		)
-		.map(ele => {
-			if (!ele.dataset.autofill) {
-				ele.dataset.autofill = v1()
-			}
+const controllAllForm = (use) => {
+  const allForm = [...document.querySelectorAll(FORMELEMENT)]
+    .filter(
+      (ele) => ele.type !== "submit" && (ele.className !== "" || ele.id !== "")
+    )
+    .map((ele) => {
+      if (!ele.dataset.autofill) {
+        ele.dataset.autofill = v1();
+      }
 
-			return ele
-		})
+      return ele;
+    });
 
-	allForm.forEach(ele => {
-		const id = ele.dataset.autofill
+  allForm.forEach((ele) => {
+    const id = ele.dataset.autofill;
 
-		if (use) {
-			const { x, y } = ele.getBoundingClientRect()
-			createFloatWindow(x, y, id)
-		} else {
-			let target = document.getElementById(id)
-			target && (target.style.display = 'none')
-		}
-	})
-}
+    if (use) {
+      const { x, y } = ele.getBoundingClientRect();
+      createFloatWindow(x, y, id);
+    } else {
+      let target = document.getElementById(id);
+      target && (target.style.display = "none");
+    }
+  });
+};
 
 // autofill 点击事件
-const onClick = e => {
-	e.target.style.backgroundColor = (enable = !enable) ? 'blue' : 'gray'
-	controllAllForm(enable)
-}
+const onClick = (e) => {
+  e.target.style.backgroundColor = (enable = !enable) ? "blue" : "gray";
+  controllAllForm(enable);
+};
 
-// 延迟处理逻辑
-// let timer = null
-// new MutationObserver(() => {
-// 	if (timer) clearTimeout(timer)
-
-// 	timer = setTimeout(() => {
-// 		const paths = injectPathForFormElement(document.body.children, '0')
-// 		restoreDataFromStorage(paths)
-// 	}, 1000)
-// }).observe(document.body, { childList: true })
-
-const paths = injectPathForFormElement(document.body.children, '0')
-restoreDataFromStorage(paths)
-createElement().addEventListener('click', onClick)
-window.addEventListener('resize', () => controllAllForm(enable))
+const paths = injectPathForFormElement(document.body.children, "0");
+restoreDataFromStorage(paths);
+createElement().addEventListener("click", onClick);
+window.addEventListener("resize", () => controllAllForm(enable));
