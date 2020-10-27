0de3c1ef83cb90dbacf26f7686a0f348418c0606
123falksdfj <changnan666@gmail.com>
2020-10-27 12:32:56 +0800

    【bugfix】路径回填

--- index.ts
+++ index.ts
@@ -42,43 +42,41 @@ const injectPathForNextElement = (
   paths: string[]
 ) => {
   if (!ele) return;
+
   if (rightFormElement(ele)) {
     let lastPath = Number(path.slice(-1)) + 1;
     let finalPath = path.slice(0, -1) + lastPath;
     paths.push(finalPath);
     ele.dataset.autofillPath = finalPath;
-  } else {
-    injectPathForNextElement(
-      ele.nextElementSibling as HTMLElement,
-      path,
-      paths
-    );
   }
+
+  injectPathForNextElement(ele.nextElementSibling as HTMLElement, path, paths);
 };
 
 /** 给所有表单元素注入 dom 层级索引 */
 const injectPathForFormElement = (
   children: HTMLElement[],
-  path: string,
+  path: string = "",
   paths: string[] = []
 ) => {
   Array.from(children).forEach((item, i) => {
     const { children: itemChild } = item;
+    let finalPath = path === "" ? i + "" : path + "-" + i;
 
     if (itemChild && itemChild.length) {
-      injectPathForFormElement(itemChild as any, path + "-" + i, paths);
+      injectPathForFormElement(itemChild as any, finalPath, paths);
     }
 
     if (!item.dataset.autofillPath) {
       if (rightFormElement(item)) {
-        paths.push(path);
-        item.dataset.autofillPath = path;
+        paths.push(finalPath);
+        item.dataset.autofillPath = finalPath;
       }
     }
 
     injectPathForNextElement(
       item.nextElementSibling as HTMLElement,
-      path,
+      finalPath,
       paths
     );
   });
@@ -288,7 +286,7 @@ const keydown = (e: KeyboardEvent) => {
 };
 
 const start = () => {
-  const paths = injectPathForFormElement(document.body.children as any, "0");
+  const paths = injectPathForFormElement(document.body.children as any);
   restoreDataFromStorage(paths);
 
   trigger = createTrigger();
